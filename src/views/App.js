8c7ebdc6229e996820a64378b7bb2e1c1b4e18ef
tkmarsten <tkmarsten@gmail.com>
2023-03-08 14:35:46 -0800

    Include class and gallery page routing

--- src/views/App.js
+++ src/views/App.js
@@ -1,24 +1,29 @@
 import NavBar from '../components/NavBar'
 import Home from './Home'
 import About from './About'
+import Class from './Class'
 import FAQ from './FAQ'
 import Terms from './Terms'
+import Gallery from './Gallery.js'
 import Footer from '../components/Footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Box } from '@chakra-ui/react'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <NavBar />
+      <NavBar />
+      <Box className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path='/class' element={<Class />} />
           <Route path="/faq" element={<FAQ />} />
           <Route path="/terms" element={<Terms />} />
+          <Route path='/gallery' element={<Gallery />} />
         </Routes>
-        <Footer />
-      </div>
+      </Box>
+      <Footer />
     </Router>
   )
 }
