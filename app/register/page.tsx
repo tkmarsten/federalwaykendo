ec34f68eb0c144cdd61caf2e89c28a3b39a9e720
tiarnanm <tkmarsten@gmail.com>
2023-12-05 01:33:06 -0800

    Add metadata

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,10 @@
 import { Card, CardHeader, CardBody, Button, Link } from "@nextui-org/react";
 import NextImage from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
 
 export default function Register() {
   const data = [
