1e7e20f93d811b211874799844fa0f90c62ad738
tiarnanm <tkmarsten@gmail.com>
2023-12-05 23:29:48 -0800

    Add FAQ page

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -6,11 +6,15 @@ export default function Contact() {
       <p className="text-sm text-secondary">Contact</p>
       <div className="text-4xl mt-2">Still have questions?</div>
       <div>
-        See our FAQ page for more answers! If the FAQ does not have what you
-        need, feel free to{" "}
+        See our{" "}
+        <Link href="/faq" underline="always" className="text-white">
+          FAQ page
+        </Link>{" "}
+        for more answers! If the FAQ does not have what you need, feel free to{" "}
         <Link
           href="mailto:federalwaykendoclub@gmail.com"
-          className="text-white underline"
+          underline="always"
+          className="text-white"
         >
           email
         </Link>{" "}
