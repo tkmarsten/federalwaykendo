d5b2a681beba2db7e773f7ad4a051978db90af98
tiarnanm <tkmarsten@gmail.com>
2023-12-08 17:51:47 -0800

    Folder restructuring, Update About page content

---
+++ app/(pages)/about/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import NextImage from "next/image";
+import SenseiTable from "@/app/components/sensei-table";
+
+export default function About() {
+  return (
+    <main className="space-y-8">
+      <div className="flex flex-col lg:flex-row-reverse items-center">
+        <div className="flex-1">
+          <NextImage
+            src="/logo.png"
+            width={200}
+            height={100}
+            alt="Logo"
+            className="m-auto"
+          />
+        </div>
+        <p className="text-5xl text-center flex-1">Federal Way Kendo Club</p>
+      </div>
+      <section>
+        <p className="text-sm text-secondary">About Us</p>
+        <p>
+          Federal Way Kendo Club was established in 2009, and was originally the
+          Puyallup Kendo Club. The club has been providing instruction of kendo
+          through the Federal Way parks department. Before the pandemic, our
+          pracices were held at Saghalie Middle School during the school year,
+          and at the community center during the summer. We have now permanently
+          located ourselves in the multipurpose rooms at the center.
+        </p>
+      </section>
+      <section>
+        <p className="text-sm text-secondary">Instructors</p>
+        <p>
+          Our head sensei both have over 40 years of kendo experience. This also
+          includes many years of experience teaching both at the local level and
+          federation level for the Pacific Northwest.
+        </p>
+        <SenseiTable />
+      </section>
+    </main>
+  );
+}
