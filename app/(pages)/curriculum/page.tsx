2ac8326bfdc116f581c7e8b7047cf4323ad8356c
tiarnanm <tkmarsten@gmail.com>
2023-12-15 23:53:48 -0800

    Add Curriculum page

---
+++ app/(pages)/curriculum/page.tsx
@@ -0,0 +1,96 @@
+export default function Curriculum() {
+  return (
+    <main>
+      <p className="text-4xl place-self-center font-semibold text-center">
+        Class Curriculum
+      </p>
+      <p>
+        Beginning <br />
+        (1-3 Months)*
+      </p>
+      <ul>
+        <li>Concepts</li>
+        <li>Using your spirit (kiai)</li>
+        <li>Posture</li>
+      </ul>
+      <ul>
+        <li>Etiquette (rei-ho)</li>
+        <li>Bowing (rei)</li>
+        <li>Meditation (moku-sou)</li>
+      </ul>
+      <ul>
+        <li>Stances</li>
+        <li>Neutral standing position (shi-zen-tai)</li>
+        <li>Silent sitting (seiza)</li>
+        <li>Crouched position (son-kyo)</li>
+      </ul>
+      <ul>
+        <li>Footwork (ashi-sabaki)</li>
+        <li>One step (okuri-ashi)</li>
+        <li>Continuous steps (renzoku-okuri-ashi)</li>
+        <li>Attack step (fumi-komi)</li>
+      </ul>
+      <ul>
+        <li>Basic swings / strikes (suburi)</li>
+        <li>Top to bottom swing (jyouge-buri)</li>
+        <li>Head strike (men)</li>
+        <li>Wrist strike (kote)</li>
+        <li>Head and wrist strike (kote-men)</li>
+        <li>Fast head strike (haya-suburi)</li>
+      </ul>
+
+      <p>
+        Intermediate <br />
+        (4-6 Months)*
+      </p>
+      <ul>
+        <li>All previous things learned</li>
+      </ul>
+      <ul>
+        <li>Swings / strikes (suburi)</li>
+        <li>Diagonal swing (naname-buri)</li>
+        <li>Diagonal head strike (sayu-men)</li>
+        <li>Torso strike (dou)</li>
+        <li>Backwards head strike (hiki-men)</li>
+      </ul>
+      <ul>
+        <li>Drills</li>
+        <li>Kiri-kaeshi</li>
+        <li>Men</li>
+        <li>Kote</li>
+        <li>Dou</li>
+        <li>Hiki-men</li>
+      </ul>
+      <ul>
+        <li>Warmups</li>
+        <li>Receiving with shinai</li>
+        <li>Preperation for uniform and armor</li>
+      </ul>
+
+      <p>
+        Advanced <br />
+        (7+ Months)
+      </p>
+      <ul>
+        <li>All previous things learned</li>
+      </ul>
+      <ul>
+        <li>Hitting and receiving in armor</li>
+      </ul>
+      <ul>
+        <li>Advanced techniques (waza)</li>
+        <li>Open hit practice (kakari-geiko)</li>
+        <li>Sparring practice (ji-geiko)</li>
+      </ul>
+      <ul>
+        <li>Preparation for grade tests and tournaments</li>
+      </ul>
+
+      <p>
+        *Classes are always subject to change based on the overall skill level,
+        age range, and progression of the students. We adjust the speed and
+        curriculum week by week to best fit the class.
+      </p>
+    </main>
+  );
+}
