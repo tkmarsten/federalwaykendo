5f602b88fcb35ad036d11b63e3037e0f8891d0a9
tiarnanm <tkmarsten@gmail.com>
2023-12-28 22:22:24 -0800

    Update curriculum content

--- app/components/timeline.tsx
+++ app/components/timeline.tsx
@@ -6,24 +6,18 @@ export default function Timeline() {
     <div className="m-auto bg-zinc-200 p-8 space-y-4">
       <div>
         <p className="text-center font-medium text-2xl">Timeline Overview</p>
-        <p>
-          Students that are progressing at a typical rate can expect the
-          following time frame from when they first begin kendo to reaching
-          armor and partcipating in a full practice. Quarters are always about 3
-          months.
-        </p>
       </div>
       <ul className={styles.timeline}>
         <li>
-          <p className="text-xl">Quarter 1</p>
+          <p className="text-lg">Quarter 1</p>
           <Link href="#beginning">Beginning Kendo</Link>
         </li>
         <li>
-          <p className="text-xl">Quarter 2 & 3*</p>
+          <p className="text-lg">Quarter 2 & 3*</p>
           <Link href="#continuing">Continuing Kendo</Link>
         </li>
         <li>
-          <p className="text-xl">Quarter 4 & Onward</p>
+          <p className="text-lg">Quarter 4 & Onward</p>
           <Link href="#advanced">Advanced Kendo</Link>
         </li>
       </ul>
